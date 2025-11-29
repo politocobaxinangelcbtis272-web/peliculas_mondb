a8258d317aa56e52fe9862a820d72ff6f3461910
politocobaxinangelcbtis272-web <politocobaxinangel.cbtis272@gmail.com>
2025-11-28 21:50:47 -0500

    Agregar aplicación completa de películas

---
+++ app.py
@@ -0,0 +1,28 @@
+﻿from flask import Flask, render_template, request, redirect, url_for, flash
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import os
+
+app = Flask(__name__)
+app.secret_key = os.environ.get('FLASK_SECRET', 'clave-secreta')
+
+MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://peliculas:peliculasi267@peliculas.0nwsmz.mongodb.net/peliculasi267?retryWrites=true&w=majority')
+client = MongoClient(MONGO_URI)
+db = client.peliculasi267
+collection = db.peliculas
+
+def to_str_id(doc):
+    if doc and '_id' in doc:
+        doc['id'] = str(doc['_id'])
+    return doc
+
+def to_str_list(cursor):
+    return [to_str_id(d) for d in cursor]
+
+@app.route('/')
+def index():
+    peliculas = to_str_list(collection.find())
+    return render_template('index.html', peliculas=peliculas)
+
+if __name__ == '__main__':
+    app.run(debug=True)
